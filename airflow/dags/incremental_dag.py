720222ac8b1f26450ad88357b2c2a0c7feb69496
Marina Zaporozhets <mazavlia@mail.ru>
2023-11-07 00:24:43 +0500

    changed10 save_df in vkparser

--- airflow/dags/incremental_dag.py
+++ airflow/dags/incremental_dag.py
@@ -196,29 +196,30 @@ class VKJobParser(BaseJobParser):
                 self.cur = self.conn.cursor()
                 self.log.info('Поиск измененных вакансий VKJobParser до парсинга описаний')
                 table_name = raw_tables[0]
-                query = query = f"SELECT vacancy_id, vacancy_name, towns, company " \
-                                f"FROM {table_name} " \
-                                f"WHERE version_vac = (SELECT max(version_vac) FROM {table_name}) " \
-                                f"ORDER BY date_of_download DESC, version_vac DESC " \
-                                f"LIMIT 1"
+
+                query = f"SELECT vacancy_id, vacancy_name, towns, company " \ 
+                        f"FROM {table_name} " \ 
+                        f"WHERE version_vac = (SELECT max(version_vac) FROM {table_name}) " \ 
+                        f"ORDER BY date_of_download DESC, version_vac DESC " \ 
+                        f"LIMIT 1"
                 self.cur.execute(query)
-                rows_in_db = self.cur.fetchall()  # получаем все строки из БД
+                rows_in_db = self.cur.fetchall()
 
-                # Создаем список для хранения индексов строк, которые нужно удалить из self.df
                 rows_to_delete = []
-
                 if rows_in_db:
                     for row_db in rows_in_db:
                         for index, row_df in self.df.iterrows():
-                            if row_db[0] == row_df['vacancy_id']:
-                                if row_db[1] == row_df['vacancy_name'] and \
-                                        row_db[2] == row_df['towns'] and  \
-                                        row_db[3] == row_df['company']:
-                                    rows_to_delete.append(index)
+                            if row_db[0].strip() == row_df['vacancy_id'].strip() and \
+                                    row_db[1].strip() == row_df['vacancy_name'].strip() and  \
+                                    row_db[2].strip() == row_df['towns'].strip() and  \
+                                    row_db[3].strip() == row_df['company'].strip():
+                                rows_to_delete.append(index)
 
-                self.df = self.df.drop(rows_to_delete)  # удаляем строки из self.df
+                    self.df = self.df.drop(rows_to_delete)
+
                 self.log.info(f'Количество вакансий для парсинга описаний вакансий: {len(self.df)}.')
                 self.cur.close()
+
         except Exception as e:
             self.log.error(f"Произошла ошибка: {e}")
 
