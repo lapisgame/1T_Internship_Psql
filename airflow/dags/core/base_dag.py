29fa080721132b99fe15916994465f16314b13b8
PolarJaba <berttalise@gmail.com>
2023-11-27 16:49:02 +0300

    added update func and DAG

---
+++ airflow/dags/core/base_dag.py
@@ -0,0 +1,60 @@
+import json
+from raw.connect_settings import conn, engine
+conn.autocommit = False
+import psycopg2
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.bash_operator import BashOperator
+import logging as log
+from logging import handlers
+from airflow.models import Variable
+from datetime import datetime, timedelta
+import time
+from airflow.utils.log.logging_mixin import LoggingMixin
+import os
+from sqlalchemy import create_engine
+from core.ddl_core import DatabaseManager
+from core.dml_core import DataManager
+import pandas as pd
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from raw.habr_career import HabrJobParser, table_name
+from raw.variables_settings import variables, base_habr
+from core.model_spacy import DataPreprocessing
+
+
+# Default dag arguments
+default_args = {
+    "owner": "admin_1T",
+    'start_date': datetime(2023, 11, 26),
+    'retry_delay': timedelta(minutes=5),
+}
+
+
+class BaseDags:
+
+    def __init__(self):
+        self.df = pd.DataFrame()
+        self.dfs = pd.DataFrame()
+
+    def update_dicts(self):
+        manager = DataManager(conn, engine, pd.DataFrame(), pd.DataFrame(), pd.DataFrame())
+        manager.work_with_static_dicts()
+
+    def model(self, df):
+        test = DataPreprocessing(df)
+        test.call_all_functions()
+        self.dfs = test.dict_all_data
+
+    def dml_core_init(self, conn, engine, dfs):
+        manager = DataManager(conn, engine, dfs, pd.DataFrame(), pd.DataFrame())
+        manager.init_load()
+
+    def dml_core_update_and_archivate(self, dfs, data_to_closed):
+        manager = DataManager(conn, engine, dfs, data_to_closed, pd.DataFrame())
+        manager.delete_archive_core_data()
+        manager.load_ad_update_actual_data()
+
---
+++ airflow/dags/core/ddl_dags.py
@@ -0,0 +1,36 @@
+from raw.connect_settings import conn, engine
+
+conn.autocommit = False
+from core.ddl_core import DatabaseManager
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+default_args = {
+    "owner": "admin_1T",
+    'start_date': datetime(2023, 11, 26),
+    'retry_delay': timedelta(minutes=5),
+}
+
+
+def ddl_core(conn):
+    manager = DatabaseManager(conn)
+    manager.db_creator()
+
+
+with DAG(
+        dag_id="ddl_core_tables",
+        schedule_interval=None,
+        tags=['admin_1T'],
+        default_args=default_args,
+        catchup=False
+) as dag_ddl_core:
+    create_core_tables = PythonOperator(
+        task_id='create_core_tables',
+        python_callable=ddl_core,
+        provide_context=True
+    )
