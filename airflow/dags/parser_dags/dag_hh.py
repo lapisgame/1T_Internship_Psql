8c59310df262b02f5fe450383ee5df67f3e53997
Marina Zaporozhets <mazavlia@mail.ru>
2023-11-29 11:25:44 +0500

    test init hh parser first test

--- airflow/dags/parser_dags/dag_hh.py
+++ airflow/dags/parser_dags/dag_hh.py
@@ -9,7 +9,7 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from raw.hh_cut_parser import HHJobParser, table_name
-from raw.variables_settings import variables, base_hh
+from raw.variables_settings import variables, base_hh, profs
 from parser_dags.base_dag import BaseDags
 
 
@@ -35,7 +35,7 @@ class Dags(BaseDags):
         """
         log.info('Запуск парсера HabrCareer')
         try:
-            parser = HHJobParser(base_hh, log, conn, table_name)
+            parser = HHJobParser(base_hh, profs, log, conn, table_name)
             parser.find_vacancies()
             parser.addapt_numpy_null()
             parser.save_df()
@@ -50,7 +50,7 @@ class Dags(BaseDags):
         """
         log.info('Запуск парсера HabrCareer')
         try:
-            parser = HHJobParser(base_hh, log, conn, table_name)
+            parser = HHJobParser(base_hh, profs, log, conn, table_name)
             parser.find_vacancies()
             parser.generating_dataframes()
             parser.addapt_numpy_null()
