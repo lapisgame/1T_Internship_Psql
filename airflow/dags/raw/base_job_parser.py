7ebe143a50033b1c99d3105db3f8c1beb4cd4de0
Marina Zaporozhets <mazavlia@mail.ru>
2023-11-29 11:19:25 +0500

    test init hh parser first test9

--- airflow/dags/raw/base_job_parser.py
+++ airflow/dags/raw/base_job_parser.py
@@ -12,7 +12,7 @@ import logging as log
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from raw.variables_settings import variables
+from raw.variables_settings import variables, profs
 
 raw_tables = variables['raw_tables']
 schemes = variables["schemes"]
