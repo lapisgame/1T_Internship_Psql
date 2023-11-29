6738183136d7d0365b2b97b20fc3bbe29b725c77
Marina Zaporozhets <mazavlia@mail.ru>
2023-11-29 11:29:57 +0500

    test init hh parser first test

--- airflow/dags/raw/base_job_parser.py
+++ airflow/dags/raw/base_job_parser.py
@@ -21,7 +21,7 @@ class BaseJobParser:
     """
     Base class for parsing job vacancies
     """
-    def __init__(self, url, profs, conn, table_name):
+    def __init__(self, url, profs, log, conn, table_name):
         self.log = log
         self.table_name = table_name
         columns = [
