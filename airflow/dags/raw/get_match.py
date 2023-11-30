df508c497b1837f9186bbd420bce8aa4f3ce0ae7
Marina Zaporozhets <mazavlia@mail.ru>
2023-12-01 00:57:11 +0500

    changed count vacancy_count7

--- airflow/dags/raw/get_match.py
+++ airflow/dags/raw/get_match.py
@@ -58,7 +58,7 @@ class GetMatchJobParser(BaseJobParser):
 
         vacancy_count = 0
         for link in self.all_links:
-            if vacancy_count < 9:
+            if vacancy_count < 17:
                 resp = requests.get(link, HEADERS)
                 vac = BeautifulSoup(resp.content, 'lxml')
 
