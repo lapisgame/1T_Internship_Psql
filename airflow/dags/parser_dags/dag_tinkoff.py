f38f01236553f224b05b6c4f5362acb7f4d1c13b
Marina Zaporozhets <mazavlia@mail.ru>
2023-12-02 20:11:25 +0500

    added tinkoffparser and test

--- airflow/dags/parser_dags/dag_tinkoff.py
+++ airflow/dags/parser_dags/dag_tinkoff.py
@@ -10,8 +10,8 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from raw.sber_job import SberJobParser, table_name
-from raw.variables_settings import variables, base_sber, profs
+from raw.tinkoff_parser import TinkoffJobParser, table_name
+from raw.variables_settings import variables, base_tin, profs
 from parser_dags.base_dag import BaseDags
 
 
@@ -29,51 +29,53 @@ default_args = {
 
 
 class Dags(BaseDags):
-    def run_init_sber_parser(self):
+    def run_init_tinkoff_parser(self):
         """
         Основной вид задачи для запуска парсера для вакансий vacancy_url
         """
-        log.info('Запуск парсера sber')
+        log.info('Запуск парсера tinkoff')
         try:
-            parser = SberJobParser(base_sber, profs, log, conn, table_name)
-            parser.find_vacancies()
+            parser = TinkoffJobParser(base_tin, profs, log, conn, table_name)
+            parser.open_all_pages()
+            parser.all_vacs_parser()
             parser.find_vacancies_description()
             parser.addapt_numpy_null()
             parser.save_df()
             parser.stop()
-            log.info('Парсер sber успешно провел работу')
+            log.info('Парсер tinkoff успешно провел работу')
             self.df = parser.df
         except Exception as e:
-            log.error(f'Ошибка во время работы парсера sber: {e}')
+            log.error(f'Ошибка во время работы парсера tinkoff: {e}')
 
-    def run_update_sber(self):
+    def run_update_tinkoff(self):
         """
-        Основной вид задачи для запуска парсера для вакансий Sber
+        Основной вид задачи для запуска парсера для вакансий GetMatch
         """
-        log.info('Запуск парсера sber')
+        log.info('Запуск парсера tinkoff')
         try:
-            parser = SberJobParser(base_sber, profs, log, conn, table_name)
-            parser.find_vacancies()
+            parser = TinkoffJobParser(base_tin, profs, log, conn, table_name)
+            parser.open_all_pages()
+            parser.all_vacs_parser()
             parser.find_vacancies_description()
             parser.generating_dataframes()
             parser.addapt_numpy_null()
             parser.update_database_queries()
-            log.info('Парсер sber успешно провел работу')
+            log.info('Парсер tinkoff успешно провел работу')
             self.dataframe_to_update = parser.dataframe_to_update
             self.dataframe_to_closed = parser.dataframe_to_closed
         except Exception as e:
-            log.error(f'Ошибка во время работы парсера sber: {e}')
+            log.error(f'Ошибка во время работы парсера tinkoff: {e}')
 
 def init_call_all_func():
     worker = Dags()
-    worker.run_init_sber_parser()
+    worker.run_init_tinkoff_parser()
     worker.update_dicts()
     worker.model(worker.df)
     worker.dml_core_init(worker.dfs)
 
 def update_call_all_func():
     worker = Dags()
-    worker.run_update_sber()
+    worker.run_update_tinkoff()
     worker.update_dicts()
     worker.archiving(worker.dataframe_to_closed)
     worker.model(worker.dataframe_to_update)
@@ -81,25 +83,25 @@ def update_call_all_func():
 
 
 with DAG(
-        dag_id="init_sber_parser",
+        dag_id="init_tinkoff_parser",
         schedule_interval=None, tags=['admin_1T'],
         default_args=default_args,
         catchup=False
-) as dag_initial_sber:
-    parse_sber_jobs = PythonOperator(
-        task_id='init_sber_task',
+) as dag_initial_tinkoff:
+    parse_tinkoff_jobs = PythonOperator(
+        task_id='init_tinkoff_task',
         python_callable=init_call_all_func,
         provide_context=True
     )
 
 with DAG(
-        dag_id="update_sber_parser",
+        dag_id="update_tinkoff_parser",
         schedule_interval=None, tags=['admin_1T'],
         default_args=default_args,
         catchup=False
-) as sber_update_dag:
-    parse_delta_sber_jobs = PythonOperator(
-        task_id='update_sber_task',
+) as tinkoff_update_dag:
+    parse_delta_tinkoff_jobs = PythonOperator(
+        task_id='update_tinkoff_task',
         python_callable=update_call_all_func,
         provide_context=True
     )
---
+++ airflow/dags/raw/tinkoff_parser.py
@@ -0,0 +1,103 @@
+from airflow.utils.task_group import TaskGroup
+import logging
+import time
+from datetime import datetime
+from airflow.utils.dates import days_ago
+from selenium.webdriver.common.by import By
+import pandas as pd
+import dateparser
+
+import sys
+import os
+sys.path.insert(0, '/opt/airflow/dags/')
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from raw.variables_settings import variables
+from raw.base_job_parser_selenium import BaseJobParserSelenium
+
+table_name = variables['raw_tables'][2]['raw_tables_name']
+
+logging.basicConfig(
+    format='%(threadName)s %(name)s %(levelname)s: %(message)s',
+    level=logging.INFO
+)
+log = logging
+
+# Default parameters
+default_args = {
+    "owner": "admin_1T",
+    'start_date': days_ago(1)
+}
+
+class TinkoffJobParser(BaseJobParserSelenium):
+    """
+    Парсер вакансий с сайта Tinkoff, наследованный от BaseJobParserSelenium
+    """
+    def open_all_pages(self):
+        self.log.info('Работает функция open_all_pages')
+        self.browser.implicitly_wait(10)
+        elements = self.browser.find_elements(By.CLASS_NAME, 'fuBQPo')
+        for element in elements:
+            element.click()
+        self.log.info('Aункция open_all_pages успешно завершена')
+
+    def all_vacs_parser(self):
+        """
+        Метод для нахождения вакансий с Tinkoff
+        """
+        try:
+            self.browser.implicitly_wait(3)
+            vacs = self.browser.find_elements(By.CLASS_NAME, 'eM3bvP')
+            for vac in vacs:
+                try:
+                    vac_info = {}
+                    vac_info['vacancy_url'] = vac.find_element(By.TAG_NAME, 'a').get_attribute('href')
+                    data = vac.find_elements(By.CLASS_NAME, 'gM3bvP')
+                    vac_info['vacancy_name'] = data[0].text
+                    vac_info['level'] = data[1].text
+                    vac_info['towns'] = data[2].text
+                    self.df.loc[len(self.df)] = vac_info
+                except Exception as e:
+                    log.error(f"Произошла ошибка: {e}, ссылка на вакансию: {vac_info['vacancy_url']}")
+
+            self.df = self.df.drop_duplicates()
+            self.log.info("Общее количество найденных вакансий после удаления дубликатов: "
+                          + str(len(self.df)) + "\n")
+            self.df['company'] = 'Тинькофф'
+            self.df['date_created'] = datetime.now().date()
+            self.df['date_of_download'] = datetime.now().date()
+            self.df['source_vac'] = 8
+            self.df['description'] = None
+            self.df['status'] = 'existing'
+            self.df['actual'] = 1
+            self.df['version_vac'] = 1
+
+            self.log.info(
+                f"Парсер завершил работу. Обработано {len(self.df)} вакансий. Оставлены только уникальные записи. "
+                f"Записи обновлены данными о компании, дате создания и загрузки.")
+
+        except Exception as e:
+            self.log.error(f"Произошла ошибка: {e}")
+
+    def find_vacancies_description(self):
+        """
+        Метод для парсинга описаний вакансий для TinkoffJobParser.
+        """
+        if not self.df.empty:
+            for descr in self.df.index:
+                try:
+                    vacancy_url = self.df.loc[descr, 'vacancy_url']
+                    self.browser.get(vacancy_url)
+                    self.browser.delete_all_cookies()
+                    self.browser.implicitly_wait(3)
+                    desc = str(self.browser.find_element(By.CLASS_NAME, 'dyzaXu').text)
+                    desc = desc.replace(';', '')
+                    self.df.loc[descr, 'description'] = desc
+
+                    self.log.info(f"Описание успешно добавлено для вакансии {descr + 1}")
+
+                except Exception as e:
+                    self.log.error(f"Произошла ошибка: {e}, ссылка {self.df.loc[descr, 'vacancy_url']}")
+                    pass
+        else:
+            self.log.info(f"Нет описания вакансий для парсинга")
