7846da71f32d5e257acd51f2673ffd02bee538de
PolarJaba <berttalise@gmail.com>
2023-12-02 19:24:58 +0300

    vk new parser added

--- airflow/dags/raw/vk.py
+++ airflow/dags/raw/vk.py
@@ -1,105 +1,142 @@
-from airflow.utils.task_group import TaskGroup
-import logging
+import config as c
+
 import time
-from datetime import datetime
-from airflow.utils.dates import days_ago
+import datetime
+import dateparser
+from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
-import sys
-import os
-sys.path.insert(0, '/opt/airflow/dags/')
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import pandas as pd
 
-from variables_settings import variables, base_vk
-from raw.base_job_parser_selenium import BaseJobParserSelenium
+profs = pd.read_csv('profs.txt', sep=',', header=None, names=['fullName'])
+url_vk = c.vk_base_link
+raw_tables = ['raw_vk', 'raw_sber', 'raw_tinkoff', 'raw_yandex']
 
-table_name = variables['raw_tables'][0]['raw_tables_name']
+start_time = time.time()
 
-logging.basicConfig(
-    format='%(threadName)s %(name)s %(levelname)s: %(message)s',
-    level=logging.INFO
-)
 
-log = logging
+class BaseJobParser:
+    def __init__(self, url, profs, df=pd.DataFrame()):
+        self.browser = webdriver.Chrome()
+        self.url = url
+        self.browser.get(self.url)
+        self.browser.maximize_window()
+        self.browser.delete_all_cookies()
+        time.sleep(2)
+        self.profs = profs
+        self.df = df
 
-# Default parameters
-default_args = {
-    "owner": "admin_1T",
-    'start_date': days_ago(1)
-}
+    def scroll_down_page(self, page_height=0):
+        """
+        Метод прокрутки страницы
+        """
+        self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
+        new_page_height = self.browser.execute_script('return document.body.scrollHeight')
+        if new_page_height > page_height:
+            self.scroll_down_page(new_page_height)
 
-class VKJobParser(BaseJobParserSelenium):
+    def stop(self):
+        """
+        Метод для выхода из Selenium Webdriver
+        """
+        self.browser.quit()
 
     def find_vacancies(self):
         """
-        Finds and records vacancies on the search page.
+        Метод для парсинга вакансий, должен быть переопределен в наследниках
         """
-        self.browser.implicitly_wait(3)
-
-        for prof in self.profs:
-            # Enter search query
-            input_button = self.browser.find_element(By.XPATH,
-                                                     '/html/body/div/div[1]/div[1]/div/form/div[1]/div[4]/div/div/div/div/input')
-            input_button.send_keys(prof)
-
-            # Click search button
-            click_button = self.browser.find_element(By.XPATH,
-                                                     '/html/body/div/div[1]/div[1]/div/form/div[1]/div[4]/div/button')
-            click_button.click()
-            time.sleep(5)
-
-            # Scroll down to the end of the page
-            self.scroll_down_page()
-
-            try:
-                # Find and parse vacancies
-                vacs_bar = self.browser.find_element(By.XPATH, '/html/body/div/div[1]/div[2]/div/div')
-                vacs = vacs_bar.find_elements(By.CLASS_NAME, 'result-item')
-                vacs = [div for div in vacs if 'result-item' in str(div.get_attribute('class'))]
-                self.log.info(f"Parsing vacancies for query: {prof}")
-                self.log.info(f"Number of vacancies: " + str(len(vacs)) + "\n")
-
-                for vac in vacs:
-                    vac_info = {}
-                    vac_info['vacancy_url'] = str(vac.get_attribute('href'))
-                    vac_info['vacancy_name'] = str(vac.find_element(By.CLASS_NAME, 'title-block').text)
-                    vac_info['towns'] = str(vac.find_element(By.CLASS_NAME, 'result-item-place').text)
-                    vac_info['company'] = str(vac.find_element(By.CLASS_NAME, 'result-item-unit').text)
-                    self.df.loc[len(self.df)] = vac_info
-
-            except Exception as e:
-                self.log.error(f"An error occurred: {e}")
-                input_button.clear()
-
-        self.df = self.df.drop_duplicates()
-        self.log.info("Total number of found vacancies after removing duplicates: " + str(len(self.df)) + "\n")
-        self.df['date_created'] = datetime.now().date()
-        self.df['date_of_download'] = datetime.now().date()
-        self.df['source_vac'] = 9
-        self.df['status'] = 'existing'
-        self.df['version_vac'] = 1
-        self.df['actual'] = 1
+        raise NotImplementedError("Вы должны определить метод find_vacancies")
 
     def find_vacancies_description(self):
         """
-        Starts parsing vacancy descriptions.
+        Метод для парсинга вакансий, должен быть дополнен в наследниках
         """
-
-        self.log.info('Start parsing vacancy descriptions')
-
-        if not self.df.empty:
+        if len(self.df) > 0:
             for descr in self.df.index:
                 try:
-                    vacancy_url = self.df.loc[descr, 'vacancy_url']
-                    self.browser.get(vacancy_url)
+                    link = self.df.loc[descr, 'link']
+                    self.browser.get(link)
                     self.browser.delete_all_cookies()
-                    time.sleep(3)
-                    desc = self.browser.find_element(By.CLASS_NAME, 'section').text
-                    desc = desc.replace(';', '')
-                    self.df.loc[descr, 'description'] = str(desc)
-
+                    self.browser.implicitly_wait(5)
+                    # Этот парсер разрабатывался для общего для 3х источников DAG'a
+                    if isinstance(self, VKJobParser):
+                        desc = self.browser.find_element(By.CLASS_NAME, 'section').text
+                        desc = desc.replace(';', '')
+                        self.df.loc[desc, 'description'] = str(desc)
                 except Exception as e:
-                    self.log.error(f"An error occurred: {e}, link {self.df.loc[descr, 'vacancy_url']}")
+                    print(f"Произошла ошибка: {e}, ссылка {self.df.loc[descr, 'link']}")
                     pass
         else:
-            self.log.info(f"No vacancies to parse")
+            print("Нет вакансий для парсинга")
+
+    def save_df(self, table):
+        """
+        Метод для сохранения данных из pandas DataFrame
+        """
+        self.df.to_csv(f"loaded_data/{table}.txt", index=False, sep=';')
+        print("Общее количество вакансий после удаления дубликатов: " + str(len(self.df)) + "\n")
+
+
+class VKJobParser(BaseJobParser):
+    """
+    Парсер вакансий с сайта VK, наследованный от BaseJobParser
+    """
+    def find_vacancies(self):
+        """
+        Метод для нахождения вакансий с VK
+        """
+        print('Старт парсинга вакансий VK')
+
+        self.df = pd.DataFrame(columns=['link', 'name', 'location', 'company', 'vacancy_date', 'date_of_download'])
+        self.browser.implicitly_wait(3)
+        # Поиск и запись вакансий на поисковой странице
+        for prof in self.profs['fullName']:
+            # input_button = self.browser.find_element(By.XPATH, '/html/body/div/main/section[2]/div/div/div[4]/input')
+            # input_button.send_keys(f"{prof}")
+            # click_button = self.browser.find_element(By.CLASS_NAME, 'ButtonPrimary_btn__jnkG1')
+            # click_button.click()
+            text_str = self.url + '?search=' + str(prof.replace(' ', '+')).lower()
+            self.browser.get(text_str)
+            self.browser.maximize_window()
+            self.browser.delete_all_cookies()
+            time.sleep(5)
+
+            # Прокрутка вниз до конца страницы
+            self.scroll_down_page()
+
+            try:
+                # vacs_bar = self.browser.find_element(By.XPATH, '/html/body/div/div[1]/div[2]/div/div')
+                vacs = self.browser.find_elements(By.CLASS_NAME, 'vacancy_vacancyItem__jrNqL')
+                vacs = [div for div in vacs if 'vacancy_vacancyItem__jrNqL' in str(div.get_attribute('class'))]
+                print(f"Парсим вакансии по запросу: {prof}")
+                print(f"Количество: " + str(len(vacs)) + "\n")
+
+                for vac in vacs:
+                    vac_info = {}
+                    vac_info['link'] = vac.get_attribute('href')
+                    print(vac_info['link'])
+                    vac_info['name'] = vac.find_element(By.CLASS_NAME, 'vacancy_vacancyItemTitleWrapper__AFMJr').text
+                    print(vac_info['name'])
+                    location_and_company = vac.find_element(By.CLASS_NAME, 'vacancy_vacancyItemDescriptionText__ntfGz')
+                    location_and_company = location_and_company.find_elements(By.TAG_NAME, 'div')
+                    vac_info['location'] = location_and_company[0].text
+                    print(vac_info['location'])
+                    vac_info['company'] = location_and_company[1].text
+                    print(vac_info['company'])
+                    self.df.loc[len(self.df)] = vac_info
+
+            except Exception as e:
+                print(f"Произошла ошибка: {e}")
+
+        self.df = self.df.drop_duplicates()
+        self.df['vacancy_date'] = pd.to_datetime('1970-01-01').date()
+        self.df['date_of_download'] = datetime.datetime.now().date()
+
+
+parser = VKJobParser(url_vk, profs)
+parser.find_vacancies()
+parser.find_vacancies_description()
+parser.save_df(raw_tables[0])
+parser.stop()
