cd61b0cdd239630672095d67c98062a2c8a8328e
Marina Zaporozhets <mazavlia@mail.ru>
2023-12-03 04:32:05 +0500

    added vseti parser and test dag4

--- airflow/dags/raw/vseti_parser.py
+++ airflow/dags/raw/vseti_parser.py
@@ -14,7 +14,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from variables_settings import variables, base_vseti
 from raw.base_job_parser import BaseJobParser
 
-table_name = variables['raw_tables'][11]['raw_tables_name']
+table_name = variables['raw_tables'][10]['raw_tables_name']
 
 
 class VsetiJobParser(BaseJobParser):
